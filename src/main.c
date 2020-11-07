104e3a1d6def997806ab63ffe2424c15a163a26c
papasmothskin <up201605469@fe.up.pt>
2020-11-07 18:37:19 +0000

    Começo da implementação da SM

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
+#include <avr/interrupt.h>
+
 
 #ifndef F_CPU
 #define F_CPU 16000000ul
@@ -20,9 +22,25 @@
 #define ADDR3 PB2
 #define ADDR4 PB3
 
+#define ST_IDDLE 0
+#define ST_REC_ADDR 1
+#define ST_REC_DATA 2
+#define ST_IGNORE 3
 
-int ADDR; // Endereço como variável global (0 se master)
+#define ST_SEND_1 1
+#define ST_SEND_2 2
+#define ST_SEND_DATA 3
 
+
+#define LOW 0
+#define HIGH 1
+
+uint8_t address = 0;			// Endereço como variável global (0 se master)
+uint8_t state = ST_IDDLE;		// Estado da máquina de estados
+uint8_t prev_led_state_1 = LOW;	// Estado anterior do led 1
+uint8_t prev_led_state_2 = LOW;	// Estado anterior do led 2
+
+{
 void init_usart(){
 	// definir baudrate
 	UBRR0H = (uint8_t)(UBBR_VAL>>8);
@@ -33,11 +51,11 @@ void init_usart(){
 	UCSR0C = (3<<UCSZ00) | (3<<UPM00) | (0<<USBS0); // 8-bits, paridade impar, 1 stop bit
 
 	// ler primeiro o endereço para ver se é cliente ou servidor
-	if (ADDR > 0){
-		UCSR0B = (1<<RXEN0) | (1<<RXB80); // Aceita receção, e leitura 9º bit
+	if (address > 0){
+		UCSR0B = (1<<RXCIE0) | (1<<RXEN0) | (1<<RXB80); // Aceita receção, e leitura 9º bit
 	}
 	else{
-		UCSR0B = (1<<TXEN0) | (1<<RXB80); // Aceita transmissão, e leitura 9º bit
+		UCSR0B = (1<<TXCIE0) | (1<<TXEN0) | (1<<RXB80); // Aceita transmissão, e leitura 9º bit
 	}
 }
 
@@ -58,7 +76,17 @@ uint8_t readButton2(){
 }
 
 void readAddress(){
-	ADDR = PINB & ((1<<ADDR1) | (1<<ADDR2) | (1<<ADDR3) | (1<<ADDR4));
+	address = PINB & ((1<<ADDR1) | (1<<ADDR2) | (1<<ADDR3) | (1<<ADDR4));
+}
+
+void sendSlave1(uint8_t button){
+
+	state = ST_SEND_1;
+}
+
+void sendSlave2(uint8_t button){
+	// Envia trama de endereço
+	state = ST_SEND_2;
 }
 
 void setup() {
@@ -73,10 +101,60 @@ void setup() {
 	readAddress();
 
 	init_usart();
+	sei();
+}
+}
+
+ISR (USART_RX_vect){
+
+}
+
+ISR (USART_TX_vect){
+	if (ST_SEND_1){
+		
+		state = ST_SEND_DATA;
+	}
+	else if (ST_SEND_2){
+
+		state = ST_SEND_DATA;
+	}
+	else if (ST_SEND_DATA){
+
+	}
 }
 
 void main() {
 	setup();
+	
+	if (address == 0){
+		uint8_t button1;
+		uint8_t button2;
+		while(1){
+			button1 = readButton1();
+			button2 = readButton2();
+			
+			if (state == ST_SEND_DATA || state == ST_SEND_1 || state == ST_SEND_2){
 
+			}
+			else if (button1 != prev_led_state_1){
+				sendSlave1(button1);
+				prev_led_state_1 = button1;
+			}
+			else if (button2 != prev_led_state_2){
+				sendSlave2(button2);
+				prev_led_state_2 = button2;
+			}
+		}
+	}
+	else {
+		state = ST_IDDLE;
+		while(1){
+			switch (state){
+				case ST_IDDLE:
+					break;
+				case ST
+			}
+		}
+	}
 	
 }
